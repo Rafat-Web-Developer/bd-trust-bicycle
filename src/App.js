7ef69ff12d3baf32ef4834f61a927b5271be4d93
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-22 08:22:32 +0600

    create my orders, profile and add review component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Blogs from "./Pages/Blogs/Blogs";
 import Navbar from "./Pages/Shared/Navbar";
 import AllParts from "./Pages/BycycleParts/AllParts";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import MyOrders from "./Pages/Dashboard/MyOrders";
+import AddReview from "./Pages/Dashboard/AddReview";
+import Profile from "./Pages/Dashboard/Profile";
 
 function App() {
   return (
@@ -12,7 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/allParts" element={<AllParts></AllParts>}></Route>
-        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}>
+          <Route index element={<MyOrders></MyOrders>}></Route>
+          <Route path="addReview" element={<AddReview></AddReview>}></Route>
+          <Route path="profile" element={<Profile></Profile>}></Route>
+        </Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
       </Routes>
     </>
