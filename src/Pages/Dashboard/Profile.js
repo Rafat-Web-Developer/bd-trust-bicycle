c1e0e3eadb457d50f9db87eb705732fd61676170
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-23 07:09:58 +0600

    Design Profile page and fix a responsive bug in navbar

--- src/Pages/Dashboard/Profile.js
+++ src/Pages/Dashboard/Profile.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Profile = () => {
+  const [user] = useAuthState(auth);
+
   return (
-    <div>
-      <h1>My Profile</h1>
-    </div>
+    <section>
+      <div className="card w-100 bg-primary text-primary-content">
+        <h2 className="text-2xl py-2 px-8 font-bold uppercase">Profile Page</h2>
+        <div className="card-body">
+          <h2 className="card-title">{user?.displayName}</h2>
+          <p>{user?.email}</p>
+          {/* <div className="card-actions justify-start">
+            <button className="btn">Buy Now</button>
+          </div> */}
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -81,6 +81,29 @@ const Navbar = () => {
             BD-TRUST-BICYCLE
           </Link>
         </div>
+        <div className="navbar-end">
+          <label
+            htmlFor="dashboardSidebar"
+            tabIndex="0"
+            className="btn btn-ghost lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+        </div>
+
         <div className="navbar-center hidden lg:flex navbar-end">
           <ul className="menu menu-horizontal p-0">{menuItems}</ul>
         </div>
