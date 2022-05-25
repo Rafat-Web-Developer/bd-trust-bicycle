de7207ae4d54767ab070894e11cfc31f4d86ac18
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-25 07:01:44 +0600

    Add more field and fix a responsive bug

--- src/Pages/Dashboard/Admin/AddProduct.js
+++ src/Pages/Dashboard/Admin/AddProduct.js
@@ -21,7 +21,7 @@ const AddProduct = () => {
         <div className="card-body flex justify-center items-center">
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border border-primary"
+            className="card flex-shrink-0 w-100 lg:w-9/12 shadow-2xl bg-base-100 border border-primary"
           >
             <div className="card-body">
               <div className="form-control">
@@ -33,7 +33,7 @@ const AddProduct = () => {
                 <input
                   type="text"
                   placeholder="product_name"
-                  className="input input-bordered"
+                  className="input input-bordered text-primary font-bold"
                   {...register("product_name", {
                     required: {
                       value: true,
@@ -49,16 +49,121 @@ const AddProduct = () => {
                   )}
                 </label>
               </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text font-bold text-primary">
-                    Product Price
+                    Product Image
                   </span>
                 </label>
                 <input
                   type="text"
+                  placeholder="Product image"
+                  className="input input-bordered text-primary font-bold"
+                  {...register("product_image", {
+                    required: {
+                      value: true,
+                      message: "Product image is required.",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.product_image?.type === "required" && (
+                    <span className="label-text-alt font-bold text-error">
+                      {errors.product_image.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-primary">
+                    Short Description
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Product description"
+                  className="input input-bordered text-primary font-bold"
+                  {...register("product_description", {
+                    required: {
+                      value: true,
+                      message: "Product description is required.",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.product_description?.type === "required" && (
+                    <span className="label-text-alt font-bold text-error">
+                      {errors.product_description.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-primary">
+                    Minimum Order Quantity
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Product minimum order quantity"
+                  className="input input-bordered text-primary font-bold"
+                  {...register("minimum_order_quantity", {
+                    required: {
+                      value: true,
+                      message: "Product minimum order quantity is required.",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.minimum_order_quantity?.type === "required" && (
+                    <span className="label-text-alt font-bold text-error">
+                      {errors.minimum_order_quantity.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-primary">
+                    Available Quantity
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Product available quantity"
+                  className="input input-bordered text-primary font-bold"
+                  {...register("available_quantity", {
+                    required: {
+                      value: true,
+                      message: "Product available quantity is required.",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.available_quantity?.type === "required" && (
+                    <span className="label-text-alt font-bold text-error">
+                      {errors.available_quantity.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-primary">
+                    Product Price (per unit price)
+                  </span>
+                </label>
+                <input
+                  type="number"
                   placeholder="product_price"
-                  className="input input-bordered"
+                  className="input input-bordered text-primary font-bold"
                   {...register("product_price", {
                     required: {
                       value: true,
