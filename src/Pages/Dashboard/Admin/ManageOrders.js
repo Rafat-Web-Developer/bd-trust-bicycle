b9e44de02647b725d5a2074f0c24d4634d004ac4
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-26 13:48:46 +0600

    Check condition in manage ordetrs

--- src/Pages/Dashboard/Admin/ManageOrders.js
+++ src/Pages/Dashboard/Admin/ManageOrders.js
@@ -1,7 +1,36 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading";
 import OrderRow from "./OrderRow";
 
 const ManageOrders = () => {
+  const navigate = useNavigate();
+  const {
+    data: allOrders,
+    isLoading,
+    refetch,
+  } = useQuery("allOrders", () =>
+    fetch("http://localhost:5000/orders", {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => {
+      if (res.status === 401 || res.status === 403) {
+        signOut(auth);
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+      }
+      return res.json();
+    })
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <section>
       <h1 className="mb-5 font-bold text-xl">Manage All Orders</h1>
@@ -10,14 +39,22 @@ const ManageOrders = () => {
           <thead>
             <tr>
               <th>#</th>
+              <th>Product's Image</th>
+              <th>User Name</th>
+              <th>User Email</th>
               <th>Product's Name</th>
               <th>Product's Price</th>
-              <th>User Email</th>
+              <th>Quantity</th>
+              <th>Transaction ID</th>
+              <th>Payment Status</th>
+              <th>Shipment Status</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            <OrderRow></OrderRow>
+            {allOrders.map((order, index) => (
+              <OrderRow key={order._id} order={order} index={index}></OrderRow>
+            ))}
           </tbody>
         </table>
       </div>
--- src/Pages/Dashboard/Admin/OrderRow.js
+++ src/Pages/Dashboard/Admin/OrderRow.js
@@ -1,14 +1,43 @@
 import React from "react";
 
-const OrderRow = () => {
+const OrderRow = ({ order, index }) => {
+  const {
+    product_image,
+    user_name,
+    user_email,
+    product_name,
+    product_price,
+    quantity,
+    payment_status,
+    transaction_id,
+    shipment_status,
+  } = order;
   return (
     <tr>
-      <th>1</th>
-      <td>Test</td>
-      <td>250</td>
-      <td>user@gmail.com</td>
+      <th>{index}</th>
       <td>
-        <button className="btn btn-primary btn-sm text-white">Confirm</button>
+        <div className="avatar">
+          <div className="mask mask-squircle w-12 h-12">
+            <img src={product_image} alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+      </td>
+      <td>{user_name}</td>
+      <td>{user_email}</td>
+      <td>{product_name}</td>
+      <td>{product_price}</td>
+      <td>{quantity}</td>
+      <td>{transaction_id}</td>
+      <td>{payment_status ? "PAID" : "UNPAID"}</td>
+      {payment_status ? (
+        <td>{shipment_status ? "shipped" : "pending"}</td>
+      ) : (
+        <td className="text-error">Payment Not Clear</td>
+      )}
+      <td>
+        {payment_status && (
+          <button className="btn btn-primary btn-sm text-white">Confirm</button>
+        )}
       </td>
     </tr>
   );
