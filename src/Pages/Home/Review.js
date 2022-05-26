e6b9cf4c60b6d62dde74122f06d5bfc165efdbfd
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-26 21:48:23 +0600

    Complete review

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const Review = () => {
+const Review = ({ review }) => {
+  const { product_image, user_name, user_email, rating } = review;
   return (
     <div className="card lg:max-w-96 bg-base-200 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src="https://api.lorem.space/image/shoes?w=400&h=225"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <img src={product_image} alt="product_image" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
-        <h3>User Name</h3>
+        <h3>{user_name}</h3>
+        <p>{user_email}</p>
+        <p>Rating {rating}</p>
         <div className="rating">
           <input
             type="radio"
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,16 +1,41 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 import Review from "./Review";
 
 const Reviews = () => {
+  const navigate = useNavigate();
+  const { data: reviews, isLoading } = useQuery("reviews", () =>
+    fetch(`https://young-dawn-47483.herokuapp.com/reviews`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => {
+      if (res.status === 401 || res.status === 403) {
+        signOut(auth);
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+      }
+      return res.json();
+    })
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <section className="my-20">
       <h1 className="mb-20 text-3xl text-center uppercase font-bold text-primary">
         Reviews
       </h1>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <Review></Review>
-        <Review></Review>
-        <Review></Review>
+        {reviews.map((review) => (
+          <Review key={review._id} review={review}></Review>
+        ))}
       </div>
     </section>
   );
